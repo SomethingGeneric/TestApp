d195a07060562a2c285c7bd9a2af84f36876f07c
Matt C <mcompton2002@gmail.com>
2021-02-11 16:12:22 -0500

    New xcode project

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Matt C on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
